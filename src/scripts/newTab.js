9e7ca2a7a7dc82f5b7a8acbccfe960bb641f30b8
Kevin <kevindubon43@gmail.com>
2023-12-26 11:41:42 -0800

    Add delete tab button to created tabs.

--- src/scripts/newTab.js
+++ src/scripts/newTab.js
@@ -1,10 +1,14 @@
 import "../styles/buttons.css";
 import { createList } from "./listTemplate";
+import del from '../svg/delete.svg';
 
 const createTab = function () {
   const projectsTab = document.getElementById("projectsTab");
   const addTabBtn = document.getElementById("addTabBtn");
   const tabsContent = {};
+  const deleteSvg = new Image();
+  deleteSvg.classList.add('deleteTab');
+  deleteSvg.src = del;
 
   addTabBtn.addEventListener("click", () => {
     let textField = document.getElementById("projectTextField");
@@ -21,12 +25,39 @@ const createTab = function () {
       textField.addEventListener("keypress", (event) => {
         if (event.key === "Enter") {
           if (textField.value.trim() !== "") {
+            const deleteSvg = new Image();
+            deleteSvg.classList.add('deleteTab');
+            deleteSvg.src = del;
+            const deleteTab = document.createElement('div');
+            deleteTab.appendChild(deleteSvg);
+            
             let newTab = document.createElement("h3");
             newTab.classList.add("userProject");
             newTab.textContent =
               textField.value.charAt(0).toUpperCase() +
               textField.value.slice(1);
-  
+            
+            
+            newTab.appendChild(deleteTab); 
+            deleteSvg.addEventListener('click', function (event) {
+              if (event.target.classList.contains('deleteTab')) {
+                const tabToDelete = event.target.closest('.userProject');
+            
+                if (tabToDelete) {
+                  const tabNameToDelete = tabToDelete.textContent;
+                  tabToDelete.remove();
+            
+                  const listToDelete = document.getElementById(tabNameToDelete);
+                  if (listToDelete) {
+                    listToDelete.remove();
+                  }
+            
+                  delete tabsContent[tabNameToDelete];
+                }
+              }
+            });
+
+            
             projectsTab.insertBefore(newTab, addTabBtn);
             tabsContent[newTab.textContent] = [];
             projectsTab.removeChild(textField);
@@ -37,9 +68,34 @@ const createTab = function () {
     }
   });
 
+
+  deleteSvg.addEventListener('click', function (event) {
+    if (event.target.classList.contains('deleteTab')) {
+      
+      const tabToDelete = event.target.closest('.userProject');
+
+      if (tabToDelete) {
+        const tabNameToDelete = tabToDelete.textContent;
+        tabToDelete.remove();
+
+        const listToDelete = document.getElementById(tabNameToDelete);
+        if (listToDelete) {
+          listToDelete.remove();
+        }
+
+        delete tabsContent[tabNameToDelete];
+      }
+    }
+  });
+
+
+  const deleteTab = document.createElement('div');
+  deleteTab.appendChild(deleteSvg);
+  
   let defaultTab = document.createElement('h3');
   defaultTab.textContent = "Today";
   defaultTab.classList.add('userProject');
+  defaultTab.appendChild(deleteTab);
   projectsTab.insertBefore(defaultTab, addTabBtn);
   tabsContent["Today"] = [];
   createList(defaultTab);
